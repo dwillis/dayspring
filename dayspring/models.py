3aa85ae3edfe5ec61d0b9507dce97b030fde4946
Derek Willis <dwillis@gmail.com>
2013-10-20 15:31:28 -0400

    fix PhoneNumberField name

--- dayspring/models.py
+++ dayspring/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from localflavor.us.models import USPhoneNumberField, USStateField
+from localflavor.us.models import PhoneNumberField, USStateField
 
 class Member(models.Model):
     last_name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=75)
-    phone = models.USPhoneNumberField()
+    phone = models.PhoneNumberField()
     address = models.CharField(max_length=255)
     state = models.USStateField()
     birth_month = models.PositiveIntegerField(null=True, blank=True)
