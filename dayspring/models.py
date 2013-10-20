1c03349c6c8d8a0b2491163d959f61c216006e43
Derek Willis <dwillis@gmail.com>
2013-10-19 21:40:06 -0400

    added initial model and localflavor

--- dayspring/models.py
+++ dayspring/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from localflavor.us.models import USPhoneNumberField, USStateField
 
-# Create your models here.
+class Member(models.Model):
+    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=75)
+    phone = models.USPhoneNumberField()
+    address = models.CharField(max_length=255)
+    state = models.USStateField()
+    birth_month = models.PositiveIntegerField(null=True, blank=True)
+    birth_day = models.PositiveIntegerField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.first_name + ' ' + self.last_name
+    
+    
