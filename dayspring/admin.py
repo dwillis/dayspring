894d937a98f51e8c596cccc04475cbcf8a0d66e0
Derek Willis <dwillis@gmail.com>
2013-10-20 15:48:39 -0400

    register the member admin

--- dayspring/admin.py
+++ dayspring/admin.py
@@ -6,3 +6,4 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'email', 'home_phone', 'cell_phone', 'day_of_birth')
     list_filter = ('birth_month', 'state')
     
+admin.site.register(Member, MemberAdmin)
