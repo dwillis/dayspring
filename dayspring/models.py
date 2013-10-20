7e2555740379ccb1dd6fe2992b0a38d2bc955564
Derek Willis <dwillis@gmail.com>
2013-10-20 15:38:10 -0400

    using localflavor correctly

--- dayspring/models.py
+++ dayspring/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from localflavor.us.models import USStateField
+from localflavor.us.models import USStateField, PhoneNumberField
 
 class Member(models.Model):
     last_name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=75)
-    home_phone = models.CharField(max_length=12)
-    cell_phone = models.CharField(max_length=12)
+    home_phone = PhoneNumberField()
+    cell_phone = PhoneNumberField()
     address = models.CharField(max_length=255)
-    state = models.USStateField()
+    state = USStateField()
     birth_month = models.PositiveIntegerField(null=True, blank=True)
     birth_day = models.PositiveIntegerField(null=True, blank=True)
     
