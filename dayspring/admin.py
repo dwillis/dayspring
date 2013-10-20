1e9bd807ee8b33f909fef572543701d0230c8056
Derek Willis <dwillis@gmail.com>
2013-10-20 16:20:57 -0400

    added admin ordering

--- dayspring/admin.py
+++ dayspring/admin.py
@@ -6,5 +6,6 @@ class MemberAdmin(admin.ModelAdmin):
     search_fields = ['last_name']
     list_display = ('__unicode__', 'email', 'home_phone', 'cell_phone', 'work_phone', 'day_of_birth')
     list_filter = ('birth_month', 'part')
+    ordering = ('last_name','first_name')
     
 admin.site.register(Member, MemberAdmin)
